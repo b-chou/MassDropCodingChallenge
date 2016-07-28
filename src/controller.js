d86bcf6b13b27c3f692fec19c41016867c0401de
Ben <b_chou@u.pacific.edu>
2016-07-27 19:59:35 -0700

    redis db storing html

--- src/controller.js
+++ src/controller.js
@@ -1,23 +1,36 @@
 const workQueue = [];
-// const fetch = require('node-fetch');
 const request = require('request');
+const storage = require('./database/redis');
 
-module.exports = {
-
-  // gets html from target url, dequeues from the work queue
-  getSiteHTML: (req, res) => {
-    request(workQueue.shift(), (error, response, body) => {
+// gets html from target url, dequeues from the work queue
+const getSiteHTML = (req, res) => {
+  if (workQueue) {
+    const currentUrl = workQueue.shift();
+    console.log(workQueue, '****');
+    request(currentUrl, (error, response, body) => {
       if (error) {
         res.status(400).send({ error: 'invalid url or failed to request html' });
       } else {
-        res.send(body);
+        storage.set(currentUrl, body);
       }
     });
-  },
+  }
+};
 
+module.exports = {
   // serves the html of a given site, must be in query format
   serveSite: (req, res) => {
-
+    let requestUrl = req.query.url;
+    if (!requestUrl.match(/^[a-zA-Z]+:\/\//)) {
+      requestUrl = `http://${requestUrl}`;
+    }
+    storage.get(requestUrl, (err, html) => {
+      if (html) {
+        res.send(html);
+      } else {
+        res.status(400).send({ error: `html not found for ${req.query.url}` });
+      }
+    });
   },
 
   // adds a new site to the queue of websites to grab
@@ -31,13 +44,15 @@ module.exports = {
       }
       res.sendStatus(300);
     } else {
-      res.status(400).send({ error: 'invalid url' });
+      res.status(400).send({ error: `${req.body.url} was an invalid url` });
     }
   },
 
   // returns the current queue
   getQueue: (req, res) => {
     res.send({ queue: workQueue });
+    getSiteHTML();
+    console.log(workQueue);
   },
 
   // changes the cron job schedule of how often a url html is saved
---
+++ src/database/redis.js
@@ -0,0 +1,19 @@
+const redis = require('redis');
+const Promise = require('bluebird');
+
+// Connection Configurations
+const PORT = '6379';
+const HOST = 'localhost';
+
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+const storage = redis.createClient(PORT, HOST);
+
+storage.on('error', (err) => {
+  console.log('Error:', err);
+});
+
+module.exports = storage;
+
+// delete all values from all db's --> storage.flushdbAsync()
