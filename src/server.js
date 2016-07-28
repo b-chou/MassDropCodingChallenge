d9f6d67bf7a7a75e2e80974ea330003df58d7844
Ben <b_chou@u.pacific.edu>
2016-07-27 22:40:06 -0700

    formatting

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.static('dev/client'));
 
 require('./routes.js')(app, express);
 
